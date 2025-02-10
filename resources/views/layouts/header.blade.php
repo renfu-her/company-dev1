fb7f5b1bc12f6576513e991ecd6d69205787b5af
Patrick Her <renfu.her@gmail.com>
2025-02-10 10:55:52 +0800

    Refactor header into separate Blade component and add HomeController

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,64 @@
+<header id='header' class="site-header" itemscope="itemscope" itemtype="https://schema.org/WPHeader">
+    <div class="container">
+        <div class="site-title">
+            <a href="index.htm" class="logo ibtn" itemprop="url">
+                <h1 itemprop="headline">台灣樂博樂博機器人教育</h1>
+            </a>
+        </div>
+        <div class="top-nav">
+            <div class="search-box">
+                <label for="PdSearch">
+                    <span class="none">SEARCH:</span>
+                    <form action="https://www.roborobo.com.tw/products_search" method="post" name="searchform"
+                        id="searchform">
+                        <input type="hidden" name="csrf_test_name" value="bca0d35ca8d97438ffd7c3e5a2458000">
+                        <input type="text" name="PdSearch" id="PdSearch" class="form-control" placeholder="SEARCH"
+                            aria-placeholder="SEARCH" value="">
+                        <input type="submit" value="SEARCH" class="ibtn search" title="產品搜尋" alt="產品搜尋">
+                    </form>
+                </label>
+            </div>
+        </div>
+        <nav class="site-nav" role="navigation" itemscope="" itemtype="https://schema.org/SiteNavigationElement">
+            <ul class="menu list-h dropdown-menu">
+                <li itemprop="name" class="hover-on "><a itemprop="url" href="about/1.html">關於我們</a>
+                    <ul>
+                        <li itemprop="name"><a itemprop="url" href="about/1.html">關於我們</a></li>
+                        <li itemprop="name"><a itemprop="url" href="about/2.html">教學理念</a></li>
+                    </ul>
+                </li>
+                <li itemprop="name" class="hover-on full_menu"><a itemprop="url" href="products/all/1.html">教育課程</a>
+                </li>
+                <li itemprop="name" class="hover-on "><a itemprop="url" href="autopage/2/6.html">國際競賽</a>
+                    <ul>
+                        <li itemprop="name" class=""><a itemprop="url" href="autopage/2/6.html">IPRC國際機器人大賽沿革</a>
+                        </li>
+                        <li itemprop="name" class=""><a itemprop="url" href="autopage/2/7.html">台灣IPRC國際機器人大賽</a>
+                        </li>
+                        <li itemprop="name" class=""><a itemprop="url" href="autopage/2/8.html">首爾IPRC機器人國際大賽</a>
+                        </li>
+                        <li itemprop="name" class=""><a itemprop="url" href="autopage/2/9.html">澳洲 WIT
+                                機器人國際大賽</a></li>
+                    </ul>
+                </li>
+                <li itemprop="name" class="hover-on "><a itemprop="url" href="service/2.html">合作項目</a>
+                    <ul>
+                        <li itemprop="name"><a itemprop="url" href="service/2.html">教育合作</a></li>
+                        <li itemprop="name"><a itemprop="url" href="service/1.html">師資培訓</a></li>
+                    </ul>
+                </li>
+                <li itemprop="name" class="hover-on "><a itemprop="url" href="news/all/1.html">最新消息</a></li>
+                <li itemprop="name" class="hover-on "><a itemprop="url" href="album/all/1.html">精彩相簿</a>
+                </li>
+                <li itemprop="name" class="hover-on "><a itemprop="url" href="contact/8.html">聯絡我們</a></li>
+                <li itemprop="name" class="hover-on "><a itemprop="url" href="autopage/3/13.html">下載專區</a>
+                    <ul>
+                        <li itemprop="name" class=""><a itemprop="url" href="autopage/3/13.html">營隊相片下載</a></li>
+                        <li itemprop="name" class=""><a itemprop="url" href="autopage/3/10.html">軟體下載</a></li>
+                        <li itemprop="name" class=""><a itemprop="url" href="autopage/3/11.html">報名表單</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+
 Route::get('/', [HomeController::class, 'index']);
